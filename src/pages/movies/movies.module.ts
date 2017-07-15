4dd0cb9ebf8123fbd05645e36e8b89185a0d0a9f
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-07-15 00:53:53 -0300

    finished sub menu

--- src/pages/movies/movies.module.ts
+++ src/pages/movies/movies.module.ts
@@ -1,7 +1,7 @@
 import { MovieProvider } from '../../providers/movie/movie';
 import { APP_CONFIG, APP_DI_CONFIG } from '../../providers/config/config';
 import { MovieDetailPage } from './movie-detail/movie-detail';
-import { MoviesPage, PopoverPage } from './movies';
+import { MoviesPage } from './movies';
 
 import {  IonicPageModule } from 'ionic-angular';
 import {  HttpModule } from '@angular/http';
@@ -11,8 +11,7 @@ import { NgModule } from '@angular/core';
 @NgModule({
   declarations: [
     MoviesPage,
-    MovieDetailPage,
-    PopoverPage
+    MovieDetailPage
   ],
   imports: [
     IonicPageModule.forChild(MoviesPage),
@@ -20,8 +19,7 @@ import { NgModule } from '@angular/core';
   ],
   entryComponents: [
     MoviesPage,
-    MovieDetailPage,
-    PopoverPage
+    MovieDetailPage
   ],
   providers: [
     { provide: APP_CONFIG, useValue: APP_DI_CONFIG},
